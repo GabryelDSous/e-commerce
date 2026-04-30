efeeebaef51653d5d7fc3b0b2fe82e285ce71b9f
Gabryel Deusino <gdeusinosousafreitas@gmail.com>
2026-04-30 14:12:36 -0300

    feat(user): add User entity with UserDetails

---
+++ src/main/java/dev/gabryel/ecommerce/model/UserModel.java
@@ -0,0 +1,52 @@
+package dev.gabryel.ecommerce.model;
+
+import dev.gabryel.ecommerce.model.enums.UserRoles;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_users")
+@Getter
+@Setter
+public class UserModel implements Serializable, UserDetails {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(name = "email", nullable = false, unique = true, length = 190)
+    private String email;
+
+    @Column(name = "password", nullable = false, length = 80)
+    private String password;
+
+    @Column(name = "name", nullable = false, length = 100)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false, length = 10)
+    private UserRoles role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+}
