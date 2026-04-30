bd530a9a25d987cb81e872424acf0d38162be0b4
Gabryel Deusino <gdeusinosousafreitas@gmail.com>
2026-04-30 14:32:53 -0300

    feat(auth): implement UserDetailsService to load by email

---
+++ src/main/java/dev/gabryel/ecommerce/service/AuthService.java
@@ -0,0 +1,21 @@
+package dev.gabryel.ecommerce.service;
+
+import dev.gabryel.ecommerce.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public AuthService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("Maybe user is not registered"));
+    }
+}
