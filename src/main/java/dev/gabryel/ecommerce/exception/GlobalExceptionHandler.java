a4659748b95a14e4881bb335df84d73205f7febf
Gabryel Deusino <gdeusinosousafreitas@gmail.com>
2026-04-30 16:53:51 -0300

    refactor(exception): centralize DataIntegrityViolationException handling with @ExceptionHandler

--- src/main/java/dev/gabryel/ecommerce/exception/GlobalExceptionHandler.java
+++ src/main/java/dev/gabryel/ecommerce/exception/GlobalExceptionHandler.java
@@ -1,15 +1,35 @@
 package dev.gabryel.ecommerce.exception;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.sql.SQLException;
 import java.time.LocalDateTime;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<ErrorMessage> handleDataIntegrity(DataIntegrityViolationException ex, HttpServletRequest request) {
+        String message = "Database error";
+
+        if (ex.getMostSpecificCause() instanceof SQLException sqlEx &&
+        "23505".equals(sqlEx.getSQLState())) {
+            message = "Entity already registered";
+        }
+        ErrorMessage errorMessage = new ErrorMessage(
+                LocalDateTime.now(),
+                HttpStatus.CONFLICT.value(),
+                HttpStatus.CONFLICT.getReasonPhrase(),
+                message,
+                request.getPathInfo()
+        );
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(errorMessage);
+    }
 
     @ExceptionHandler(UserException.class)
     public ResponseEntity<ErrorMessage> handleUserException(UserException ex, HttpServletRequest request) {
@@ -22,4 +42,5 @@ public class GlobalExceptionHandler {
         );
         return ResponseEntity.status(ex.getStatus()).body(errorMessage);
     }
+
 }
--- src/main/java/dev/gabryel/ecommerce/service/UserService.java
+++ src/main/java/dev/gabryel/ecommerce/service/UserService.java
@@ -1,18 +1,13 @@
 package dev.gabryel.ecommerce.service;
 
 import dev.gabryel.ecommerce.dto.user.request.UserRegisterRequest;
-import dev.gabryel.ecommerce.exception.UserException;
 import dev.gabryel.ecommerce.mapper.UserMapper;
 import dev.gabryel.ecommerce.model.UserModel;
 import dev.gabryel.ecommerce.model.enums.UserRoles;
 import dev.gabryel.ecommerce.repository.UserRepository;
 import jakarta.transaction.Transactional;
-import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import java.sql.SQLException;
 
 @Service
 public class UserService {
@@ -27,19 +22,12 @@ public class UserService {
     @Transactional
     public String userRegister(UserRegisterRequest userRequest, int accountType) {
         UserModel userModel = UserMapper.toUserModel(userRequest);
-        try {
-            userModel.setPassword(passwordEncoder.encode(userRequest.password()));
-            switch (accountType) {
-                case 1 -> userModel.setRole(UserRoles.ADMIN);
-                case 2 -> userModel.setRole(UserRoles.USER);
-            }
-            userRepository.save(userModel);
-        } catch (DataIntegrityViolationException e) {
-            if (e.getMostSpecificCause() instanceof SQLException sqlEx &&
-            "23505".equals(sqlEx.getSQLState())) {
-                throw new UserException("User already registered", HttpStatus.CONFLICT.value());
-            }
+        userModel.setPassword(passwordEncoder.encode(userRequest.password()));
+        switch (accountType) {
+            case 1 -> userModel.setRole(UserRoles.ADMIN);
+            case 2 -> userModel.setRole(UserRoles.USER);
         }
+        userRepository.save(userModel);
         return userModel.getEmail();
     }
 }
