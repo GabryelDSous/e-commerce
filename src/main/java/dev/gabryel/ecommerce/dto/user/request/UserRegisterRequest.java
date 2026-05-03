962f32f7ab3d8f802c6ae42578847e89ccf72482
Gabryel <gdeusinosousafreitas@gmail.com>
2026-05-03 12:29:25 -0300

    feat(user-dto): add email format validation on requests DTO with @Email annotation

--- src/main/java/dev/gabryel/ecommerce/dto/user/request/UserRegisterRequest.java
+++ src/main/java/dev/gabryel/ecommerce/dto/user/request/UserRegisterRequest.java
@@ -1,8 +1,11 @@
 package dev.gabryel.ecommerce.dto.user.request;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
-public record UserRegisterRequest(@NotBlank(message = "Please enter with your email") String email,
+public record UserRegisterRequest(@NotBlank(message = "Please enter with your email")
+                                  @Email(message = "Invalid email format")
+                                  String email,
                                   @NotBlank(message = "Please enter with your password") String password,
                                   @NotBlank(message = "Please enter with your name") String name) {
 }
--- src/main/java/dev/gabryel/ecommerce/dto/user/request/UserUpdateEmailRequest.java
+++ src/main/java/dev/gabryel/ecommerce/dto/user/request/UserUpdateEmailRequest.java
@@ -1,7 +1,10 @@
 package dev.gabryel.ecommerce.dto.user.request;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
-public record UserUpdateEmailRequest(@NotBlank(message = "Please enter with your email") String newEmail,
+public record UserUpdateEmailRequest(@NotBlank(message = "Please enter with your email")
+                                     @Email(message = "Invalid email format")
+                                     String newEmail,
                                      @NotBlank(message = "Please enter with your password") String password) {
 }
