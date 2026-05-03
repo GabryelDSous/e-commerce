8df01e844428847b6e9645d2b3bd1f59fe8da8ae
Gabryel <gdeusinosousafreitas@gmail.com>
2026-05-03 12:40:54 -0300

    feat(user-dto): add password validation on user request DTOs with @Size annotation min and max value defined

--- src/main/java/dev/gabryel/ecommerce/dto/user/request/UserLoginRequest.java
+++ src/main/java/dev/gabryel/ecommerce/dto/user/request/UserLoginRequest.java
@@ -2,9 +2,12 @@ package dev.gabryel.ecommerce.dto.user.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record UserLoginRequest(@NotBlank(message = "Please enter with your email")
                                @Email(message = "Invalid email format")
                                String email,
-                               @NotBlank(message = "Please enter with your password") String password) {
+                               @NotBlank(message = "Please enter with your password")
+                               @Size(min = 8, max = 75, message = "Password most have at least 8 characters")
+                               String password) {
 }
