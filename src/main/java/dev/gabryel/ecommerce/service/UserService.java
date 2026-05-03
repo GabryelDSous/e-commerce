6e145722c688a3bfa0b7b2772b4b596f54eac1d0
Gabryel <gdeusinosousafreitas@gmail.com>
2026-05-03 12:46:46 -0300

    feat(user-service): add user service to update email

--- src/main/java/dev/gabryel/ecommerce/service/UserService.java
+++ src/main/java/dev/gabryel/ecommerce/service/UserService.java
@@ -1,14 +1,19 @@
 package dev.gabryel.ecommerce.service;
 
+import dev.gabryel.ecommerce.config.JWTUserData;
 import dev.gabryel.ecommerce.config.TokenConfig;
 import dev.gabryel.ecommerce.dto.user.request.UserLoginRequest;
 import dev.gabryel.ecommerce.dto.user.request.UserRegisterRequest;
+import dev.gabryel.ecommerce.dto.user.request.UserUpdateEmailRequest;
+import dev.gabryel.ecommerce.exception.UserException;
 import dev.gabryel.ecommerce.mapper.UserMapper;
 import dev.gabryel.ecommerce.model.UserModel;
 import dev.gabryel.ecommerce.model.enums.UserRoles;
 import dev.gabryel.ecommerce.repository.UserRepository;
 import jakarta.transaction.Transactional;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -45,5 +50,15 @@ public class UserService {
         );
         UserModel userModel = (UserModel) authenticationManager.authenticate(userAndPass).getPrincipal();
         return tokenConfig.generateToken(userModel);
+    }
+
+    @Transactional
+    public String userUpdate(JWTUserData userData, UserUpdateEmailRequest userRequest) {
+        UserModel userModel = userRepository.findByEmail(userData.email())
+                .orElseThrow(() -> new UserException("User logged does not found", HttpStatus.NOT_FOUND.value()));
+        if (!passwordEncoder.matches(userRequest.password(), userModel.getPassword()))
+            throw new BadCredentialsException("Wrong password");
+        userModel.setEmail(userRequest.newEmail());
+        return userModel.getEmail();
     }
 }
