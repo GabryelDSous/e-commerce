f4826e76fa5c9f76ed530e8e1e0c43547dbf9b64
Gabryel Deusino <gdeusinosousafreitas@gmail.com>
2026-05-07 14:07:29 -0300

    feat(security): add MethodSecurityExpressionHandler bean with role hierarchy

--- src/main/java/dev/gabryel/ecommerce/config/SecurityConfig.java
+++ src/main/java/dev/gabryel/ecommerce/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package dev.gabryel.ecommerce.config;
 import jakarta.servlet.DispatcherType;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
+import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -64,8 +66,15 @@ public class SecurityConfig {
     @Bean
     public RoleHierarchy roleHierarchy() {
         return RoleHierarchyImpl.fromHierarchy("""
-                ADMIN > USER
+                ROLE_ADMIN > ROLE_USER
                 """);
+    }
+
+    @Bean
+    public MethodSecurityExpressionHandler methodSecurityExpressionHandler(RoleHierarchy roleHierarchy) {
+        DefaultMethodSecurityExpressionHandler handler = new DefaultMethodSecurityExpressionHandler();
+        handler.setRoleHierarchy(roleHierarchy);
+        return handler;
     }
 
     @Bean
