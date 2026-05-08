5d29610ac8e4d16d4eb58b3317addee96260d95f
Gabryel Deusino <gdeusinosousafreitas@gmail.com>
2026-05-08 14:31:32 -0300

    feat(product-repository): add query to decrease product stock

--- src/main/java/dev/gabryel/ecommerce/repository/ProductRepository.java
+++ src/main/java/dev/gabryel/ecommerce/repository/ProductRepository.java
@@ -3,6 +3,9 @@ package dev.gabryel.ecommerce.repository;
 import dev.gabryel.ecommerce.model.ProductModel;
 import dev.gabryel.ecommerce.model.enums.ProductStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
@@ -15,4 +18,12 @@ public interface ProductRepository extends JpaRepository<ProductModel, UUID> {
     List<ProductModel> findByDescriptionContainingIgnoreCase(String description);
     List<ProductModel> findByPriceBetween(BigDecimal minPrice, BigDecimal maxPrice);
     List<ProductModel> findByStatus(ProductStatus status);
+    @Modifying
+    @Query("""
+            UPDATE ProductModel p
+            SET p.stock = p.stock - :qty
+            WHERE p.name = :name
+            AND p.stock >= :qty
+            """)
+    int decreaseStock(@Param(value = "name") String name, @Param(value = "qty") int qty);
 }
