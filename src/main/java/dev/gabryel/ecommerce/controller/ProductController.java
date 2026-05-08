22b1f719d978dc0b197b7d32d9644bffbe901945
Gabryel Deusino <gdeusinosousafreitas@gmail.com>
2026-05-08 14:03:34 -0300

    feat(product-controller): add product registration for admins user with JWT Authentication

---
+++ src/main/java/dev/gabryel/ecommerce/controller/ProductController.java
@@ -0,0 +1,33 @@
+package dev.gabryel.ecommerce.controller;
+
+import dev.gabryel.ecommerce.config.JWTUserData;
+import dev.gabryel.ecommerce.dto.product.request.ProductRegisterRequest;
+import dev.gabryel.ecommerce.dto.product.response.ProductRegisterResponse;
+import dev.gabryel.ecommerce.service.ProductService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+class ProductController {
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @PostMapping("/register-product")
+    public ResponseEntity<ProductRegisterResponse> productRegister(@AuthenticationPrincipal JWTUserData userData,
+                                                                   @RequestBody @Valid ProductRegisterRequest productRequest) {
+        ProductRegisterResponse productResponse = productService.productRegister(userData, productRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productResponse);
+    }
+}
