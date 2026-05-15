fe4e9634d379927b3600b12cd273aa96bf535a0d
Gabryel Deusino <gdeusinosousafreitas@gmail.com>
2026-05-15 15:42:12 -0300

    feat(purchase-model): add PurchaseModel entity with purchase metadata

---
+++ src/main/java/dev/gabryel/ecommerce/model/PurchaseModel.java
@@ -0,0 +1,39 @@
+package dev.gabryel.ecommerce.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_purchases")
+@Getter
+@Setter
+public class PurchaseModel implements Serializable {
+    private static final long serialVersionID = 1L;
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(name = "user_id", nullable = false)
+    private UUID userId;
+    @Column(name = "user_name", nullable = false)
+    private String userName;
+
+    @Column(name = "product_id", nullable = false)
+    private UUID productId;
+    @Column(name = "product_name", nullable = false)
+    private String productName;
+
+    @CreationTimestamp
+    @Column(name = "purchase_date", nullable = false)
+    private LocalDateTime purchaseDate;
+
+    @Column(name = "quantity", nullable = false)
+    private Integer quantity;
+}
