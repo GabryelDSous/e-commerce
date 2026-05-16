9ef43fab11c2ba02df9bedaaed5191419b2d31ea
Gabryel Deusino <gdeusinosousafreitas@gmail.com>
2026-05-15 23:51:01 -0300

    feat(purchase-mapper): add mapping for PurchaseModel to PurchaseProductResponse conversion

---
+++ src/main/java/dev/gabryel/ecommerce/mapper/PurchaseMapper.java
@@ -0,0 +1,26 @@
+package dev.gabryel.ecommerce.mapper;
+
+import dev.gabryel.ecommerce.dto.purchase.response.PurchaseProductResponse;
+import dev.gabryel.ecommerce.model.ProductModel;
+import dev.gabryel.ecommerce.model.PurchaseModel;
+import dev.gabryel.ecommerce.model.UserModel;
+
+import java.time.LocalDateTime;
+
+public class PurchaseMapper {
+    public static PurchaseModel toPurchaseModel(UserModel userModel, ProductModel productModel) {
+        PurchaseModel purchaseModel = new PurchaseModel();
+        purchaseModel.setUserId(userModel.getId());
+        purchaseModel.setUserName(userModel.getName());
+        purchaseModel.setProductId(productModel.getId());
+        purchaseModel.setProductName(productModel.getName());
+        purchaseModel.setPurchaseDate(LocalDateTime.now());
+        return purchaseModel;
+    }
+    public static PurchaseProductResponse toPurchaseProductResponse(PurchaseModel purchaseModel) {
+        return new PurchaseProductResponse(
+                purchaseModel.getProductName(), purchaseModel.getUserName(),
+                purchaseModel.getPurchaseDate(), purchaseModel.getQuantity()
+        );
+    }
+}
