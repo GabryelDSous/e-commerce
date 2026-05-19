15b61a9623c8e9af6a5f7b3251b84e6bf3f99b7f
Gabryel <gdeusinosousafreitas@gmail.com>
2026-05-19 18:55:17 -0300

    feat(purchase-repository): add findUserModel on PurchaseRepository

--- src/main/java/dev/gabryel/ecommerce/repository/PurchaseRepository.java
+++ src/main/java/dev/gabryel/ecommerce/repository/PurchaseRepository.java
@@ -1,6 +1,7 @@
 package dev.gabryel.ecommerce.repository;
 
 import dev.gabryel.ecommerce.model.PurchaseModel;
+import dev.gabryel.ecommerce.model.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +10,5 @@ import java.util.UUID;
 
 @Repository
 public interface PurchaseRepository extends JpaRepository<PurchaseModel, UUID> {
+    List<PurchaseModel> findByUserModel(UserModel userModel);
 }
